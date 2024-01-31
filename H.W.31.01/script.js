15985d5142bf99e1c795d4dbac5ecddd1f872d52
JantschenkoArtem <156689626+JantschenkoArtem@users.noreply.github.com>
2024-01-31 20:19:07 +0100

    Add files via upload

---
+++ H.W.31.01/script.js
@@ -0,0 +1,39 @@
+// 1.
+
+// const num1 = prompt("Ihr Numer")
+// const num2 = 10
+// console.log(num1 / 100 * 10)
+
+// 2.
+// const num1 = 1
+// const num2 = 2
+// const sum1 = num1 + num2
+// const sum2 = num1 * num2
+// const sum3 = Math.min(sum1,sum2)
+// console.log(sum3)
+
+
+// 3.
+// const number = prompt("YourNumber")
+//     console.log(number)
+// if(number > 100){
+//     console.log("Число больше 100")
+// }
+// else if(number == 100){
+//     console.log("Число равно 100")
+// }
+// else{
+//     console.log("Число меньше 100")
+// }
+
+// 4.
+// const Name = prompt("Your Name")
+// const Old = +prompt("Date of Birth")
+// const Today = ("2014")
+// const sum = Today - Name
+// if (sum > 18){
+//     console.log(`Hello: ${Name}`)
+// }
+// else{
+//     console.log(`Hi: ${Name}`)
+// }
